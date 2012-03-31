8ecb447c1231757e47bfec592a3395cfb2be32ab
Jim Safley <jimsafley@gmail.com>
2012-03-31 18:33:10 -0400

    Optimize SPARQL.

--- reincarnation.py
+++ reincarnation.py
@@ -20,22 +20,18 @@ data = urlencode({
     'query': '''
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
-PREFIX dcterms: <http://purl.org/dc/terms/>
-SELECT ?person ?name ?birth_date ?death_date
+SELECT ?person ?name ?birth_date
 WHERE {{
   ?person a foaf:Person ;
-          dcterms:subject <http://dbpedia.org/resource/Category:{0}_deaths> ;
           foaf:name ?name ;
-          dbpedia-owl:birthDate ?birth_date ;
           dbpedia-owl:deathDate ?death_date .
-  FILTER regex(?death_date, "{1}")
+  OPTIONAL {{ ?person dbpedia-owl:birthDate ?birth_date }}
+  FILTER regex(?death_date, "^{0}")
 }}
-ORDER BY ?death_date'''.format(birth_date.year, birth_date)
-})
+ORDER BY ?birth_date'''.format(birth_date)})
 people = json.loads(urllib2.urlopen('http://dbpedia.org/sparql', data).read())
 
 for p in people['results']['bindings']:
     person = {'uri': p['person']['value'], 'name': p['name']['value'], 
-              'birth_date': date(*map(int, p['birth_date']['value'].split('-'))), 
-              'death_date': date(*map(int, p['death_date']['value'].split('-')))}
-    print u'{birth_date}–{death_date} {name} <{uri}>'.format(**person)
+              'birth_date': date(*map(int, p['birth_date']['value'].split('-')))}
+    print u'{birth_date} {name} <{uri}>'.format(**person)
